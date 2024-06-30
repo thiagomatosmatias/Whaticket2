189d9aea986e9522e7c20cde432cc0d775104fdb
thiagomatosmatias <agenciaenterup@gmail.com>
2024-06-29 22:41:03 -0300

    Update App.js
    Troca da Cor da Barra SUPERIOR

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -73,7 +73,7 @@ const App = () => {
                 total: mode === "light" ? "#fff" : "#222",
                 messageIcons: mode === "light" ? "grey" : "#F3F3F3",
                 inputBackground: mode === "light" ? "#FFFFFF" : "#333",
-                barraSuperior: mode === "light" ? "linear-gradient(to right, #6D30EF, #6D30EF , #6D30EF)" : "#666",
+                barraSuperior: mode === "light" ? "linear-gradient(to right, #3d3d3d, #3d3d3d , #3d3d3d)" : "#666",
 				boxticket: mode === "light" ? "#EEE" : "#666",
 				campaigntab: mode === "light" ? "#ededed" : "#666",
 				mediainput: mode === "light" ? "#ededed" : "#1c1c1c",
